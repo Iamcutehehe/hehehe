b655af51ac8c0272c7c840d2451548d6acdf26fa
Iamcute <98321897+Iamcutehehe@users.noreply.github.com>
2024-07-17 14:42:42 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -23,7 +23,7 @@ return {
 "02076234-0f0a-4801-9c84-064f91fa8972", -- Empty
 "dd152e5c-7ad8-492c-a7fb-95a647ed06c8", -- Minh Phạm
 "fd692923-5a48-4e9a-ad77-2145b85384ab", -- Nguyên Phúc
-"82a73ab9-9ae6-467d-a101-e72618e8cdb4", -- Minh Chính
+"b24ba384-689a-45d2-9426-b64875b853b3", -- Minh Chính
 "af8f157c-a6c0-4ac1-a72a-9ec86d7f59db", -- Nguyễn Bin
 "d79562d2-6790-44bd-b86c-2e55de766695", -- Bùi Đức An
 "e31abbae-19ca-4f90-9304-8ccc04fa17f6", -- Trần Huy Quang
