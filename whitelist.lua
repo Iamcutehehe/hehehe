a7edf4c7711bcfc5ea6164eb063d2b34fa4fcebf
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2024-07-22 06:24:18 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -21,7 +21,7 @@ return {
 "0",-- Iren PE
 "0", -- Empty
 "0", -- Empty
-"38f7bbbb-8254-4616-aee9-4cc7e8b01619", -- Minh Phạm
+"5e241a5a-f30c-4ffb-9dab-ed8e8e89b05f", -- Minh Phạm
 "5283d1c7-2322-4eab-9447-82dd4472e061", -- Nguyên Phúc
 "02193af8-e0ed-49ea-99e6-65fb0f4aa085", -- Minh Chính
 "af8f157c-a6c0-4ac1-a72a-9ec86d7f59db", -- Nguyễn Bin
