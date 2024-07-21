40ae39b6048645e575b1e0d893c6181deef7f714
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2024-07-21 16:49:58 +0700

    Update wlist.lua

--- wlist.lua
+++ wlist.lua
@@ -1,5 +1,6 @@
 return {
-1322815101, --Irenkiss
+1322815101, -- azpro2k7
+4027558815, -- accquaydffirstsea1
 5089621002, -- Trần Huy Quang
 1262972775, -- Nguyễn Văn Bảo Khánh
 1141261219 -- Hiếu Trung
