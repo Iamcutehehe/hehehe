44009a42daa95c8efb0b6e0661f5259eebc2e77d
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2024-10-08 13:16:45 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -20,7 +20,7 @@ return {
 "0", -- Lê Trường Giang
 "20f94b66-74c2-460d-a84a-c9be55db98ef", -- Lê Trường Giang 2
 "25cb3892-3dbb-4e2f-a536-1f23b14de04d", -- Iren PE
-"39a4af80-88e0-4293-a624-282df751a2ac", -- Văn Hiếu
+"40d949df-9591-4a1d-bd81-e5003da95a1f", -- Xiao Nima 2
 "d1075268-650c-480f-af10-a56fefdb804d", -- Văn Hiếu 2
 --"0", -- Minh Phạm
 "01f18016-fe95-4198-965a-c5c3747f8aba", -- Nguyên Phúc
