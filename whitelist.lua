9bb9aa8c92fce99cf4bce1f419ab3e0c1d5a82b5
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2024-10-11 20:47:27 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -36,7 +36,7 @@ return {
 "0", -- Võ Lâm Huy
 "937e53af-35b9-4092-b5e5-d739b1cf5f68", -- Quang Trung
 "dfd61e8c-ae69-4dc5-83b7-42828407c988", -- Ly Nhien
-"18e1d263-08e7-405f-9dce-1b9f36fcb00f", -- Nguyen Dat
+"71f8c1fd-25b3-4b58-831a-ee226d203f6a", -- Nguyen Dat
 "8b91e3a1-dbde-42c3-8327-2ecaecebfb83", -- Quang Huy
 "83a2c7c6-32f7-45d2-bfa1-18959488bb2c", -- Hieu Quyet Vu
 "2a937f57-f59a-421d-b459-ef1786de2ed2", -- Phạm Long
