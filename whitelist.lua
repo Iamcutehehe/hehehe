3b0b349a71e63128516431ad68dee7d49d9ad788
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2025-04-14 18:24:02 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -19,7 +19,6 @@ return {
 "0", -- Lê Trường Giang 2
 "25cb3892-3dbb-4e2f-a536-1f23b14de04d", -- Iren PE
 "d1075268-650c-480f-af10-a56fefdb804d", -- Văn Hiếu 2
---"0", -- Minh Phạm
 "01f18016-fe95-4198-965a-c5c3747f8aba", -- Nguyên Phúc
 "3c906eb3-c9df-43b8-ab03-d695434c194b", -- Minh Chính
 "f9588228-e58c-4ce2-9244-bb51f2449911", -- Nguyễn Bin
